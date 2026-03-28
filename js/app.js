c45cb56fab9a4a28cc81abef8efec39b4ddf7e9f
TuMinipeka <danielsantiagomayorga03@gmail.com>
2026-03-28 16:26:43 -0500

    feat: :bug: fix Bug3

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 const boton=document.getElementById("buttonAdd");
 
+var alumnos =[]
+
 
 boton.addEventListener("click", function(event){
     event.preventDefault()
     const inputValue=document.getElementById("nombre").value;
-    const certamen1=document.getElementById("certamen1").value;
-    const certamen2=document.getElementById("certamen2").value;
-    const certamen3=document.getElementById("certamen3").value;
-
-    const p =createElement("p")
-    p.innerHTML=`Nombre: ${inputValue}`
-    
+    let contador = alumnos.push(inputValue);
+    console.log(contador)
+    if (contador==10){
+        boton.hidden=true;
+    }
+    console.log(alumnos)
 });
