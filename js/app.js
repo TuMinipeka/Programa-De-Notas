732eed8aa2789b6a287699403528f556203c42c6
TuMinipeka <danielsantiagomayorga03@gmail.com>
2026-03-28 15:26:44 -0500

    feat: :sparkles: add resultado-campo

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const nombres=document.getElementById("nombre-resultado");
+const nota1=document.getElementById("c1-nombre")
+const nota2=document.getElementById("c1-nombre")
+const nota3=document.getElementById("c1-nombre")
+
+
+let nombre = [[nombres],[10]]
+let notas =[]
